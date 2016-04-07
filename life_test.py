9fc188c66c0a45ad2bd7f03f8b6987cefd62eaf6
Michael Patterson <mjpatter88@gmail.com>
2016-04-06 22:44:03 -0500

    RED: pft - next returns blank board when no init.

--- life_test.py
+++ life_test.py
@@ -2,8 +2,15 @@ import unittest
 
 class TestLife(unittest.TestCase):
 
-    def test_first_test(self):
-        self.assertEqual(1, 2, 'Failing test')
+    def test_next__returns_blank_board_of_requested_dimensions_if_no_starting_items(self):
+        width = 20
+        height = 10
+        life = Life()
+
+        next_board = life.next(width, height)
+        expected_board = [[0]*width]*height
+
+        self.assertEqual(next_board, expected_board, 'Next() did not return a blank board.')
 
 
 
