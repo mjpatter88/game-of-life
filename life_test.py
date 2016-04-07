baca0facc243c9167b9e3582bf29cdc159e69696
Michael Patterson <mjpatter88@gmail.com>
2016-04-07 09:37:09 -0500

    RED: pft - life inits with list of starting cells.

--- life_test.py
+++ life_test.py
@@ -3,17 +3,26 @@ import unittest
 from life import Life
 
 class TestLife(unittest.TestCase):
+    def setUp(self):
+        self.width = 20
+        self.height = 10
 
     def test_next__returns_blank_board_of_requested_dimensions_if_no_starting_items(self):
-        width = 20
-        height = 10
         life = Life()
 
-        next_board = life.next(width, height)
-        expected_board = [[0]*width]*height
+        next_board = life.next(self.width, self.height)
+        expected_board = [[0]*self.width]*self.height
 
         self.assertEqual(next_board, expected_board, 'Next() did not return a blank board.')
 
+    def test_next__returns_blank_board_if_all_starting_cells_dead(self):
+        starting_cells = []
+        life = Life(starting_cells)
+
+        next_board = life.next(self.width, self.height)
+        expected_board = [[0]*self.width]*self.height
+
+        self.assertEqual(next_board, expected_board, 'Next() did not return a blank board.')
 
 
 if __name__ == '__main__':
