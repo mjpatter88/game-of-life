300d0deea83bdf391e95763bb63ca83a62bf704d
Michael Patterson <mjpatter88@gmail.com>
2016-04-07 21:45:32 -0500

    RED: Test one life cell with 2 neighbors lives.

--- life_test.py
+++ life_test.py
@@ -6,23 +6,31 @@ class TestLife(unittest.TestCase):
     def setUp(self):
         self.width = 20
         self.height = 10
+        self.expected_board = [[0]*self.width]*self.height
 
     def test_next__returns_blank_board_of_requested_dimensions_if_no_starting_items(self):
         life = Life()
 
         next_board = life.next(self.width, self.height)
-        expected_board = [[0]*self.width]*self.height
 
-        self.assertEqual(next_board, expected_board, 'Next() did not return a blank board.')
+        self.assertEqual(next_board, self.expected_board, 'Next() did not return a blank board.')
 
     def test_next__returns_blank_board_if_all_starting_cells_dead(self):
         starting_cells = []
         life = Life(starting_cells)
 
         next_board = life.next(self.width, self.height)
-        expected_board = [[0]*self.width]*self.height
 
-        self.assertEqual(next_board, expected_board, 'Next() did not return a blank board.')
+        self.assertEqual(next_board, self.expected_board, 'Next() did not return a blank board.')
+
+    def test_next__one_live_cell_with_two_live_neighbors_lives(self):
+        starting_cells = [(1,4), (1,5), (1,6)]
+        life = Life(starting_cells)
+
+        next_board = life.next(self.width, self.height)
+        self.expected_board[1][5] = 1
+
+        self.assertEqual(next_board, self.expected_board, 'Next() did not return a blank board.')
 
 
 if __name__ == '__main__':
