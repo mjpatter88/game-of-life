27c275fe45f1caeeeb265e0a09d176590ef82c8d
Michael Patterson <mjpatter88@gmail.com>
2016-04-08 20:43:39 -0500

    Green: Fix bug in 2d list logic. Cell that was alive and has 2 live neighbors lives.

--- life.py
+++ life.py
@@ -1,7 +1,45 @@
+from collections import defaultdict
+
 class Life():
     def __init__(self, starting_cells=None):
-        pass
+        if starting_cells:
+            self.cells = starting_cells
+        else:
+            self.cells = []
 
     def next(self, width, height):
-        board = [[0]*width]*height
+        cell_neighbors = defaultdict(int)
+        for cell in self.cells:
+            self._mark_neighbors(cell, cell_neighbors)
+
+        next_cells = []
+        for cell, neighbor_count in cell_neighbors.items():
+            if self._is_alive(cell) and neighbor_count == 2:
+                next_cells.append(cell)
+
+        board = _make_blank_board(width, height)
+        for x, y in next_cells:
+            board[x][y] = 1
+
         return board
+
+    def _mark_neighbors(self, cell, cell_neighbors):
+        x, y = cell
+        cell_neighbors[(x+1, y+1)]+=1
+        cell_neighbors[(x+1, y)]+=1
+        cell_neighbors[(x+1, y-1)]+=1
+        cell_neighbors[(x, y+1)]+=1
+        cell_neighbors[(x, y-1)]+=1
+        cell_neighbors[(x-1, y-1)]+=1
+        cell_neighbors[(x-1, y)]+=1
+        cell_neighbors[(x-1, y-1)]+=1
+
+    def _is_alive(self, cell):
+        return cell in self.cells
+
+def _make_blank_board(width, height):
+    board = []
+    for row in range(height):
+        new_row = [0] * width
+        board.append(new_row)
+    return board
--- life_test.py
+++ life_test.py
@@ -4,9 +4,12 @@ from life import Life
 
 class TestLife(unittest.TestCase):
     def setUp(self):
-        self.width = 20
-        self.height = 10
-        self.expected_board = [[0]*self.width]*self.height
+        self.width = 5
+        self.height = 4
+        self.expected_board = []
+        for row in range(self.height):
+            new_row = [0] * self.width
+            self.expected_board.append(new_row)
 
     def test_next__returns_blank_board_of_requested_dimensions_if_no_starting_items(self):
         life = Life()
@@ -24,11 +27,11 @@ class TestLife(unittest.TestCase):
         self.assertEqual(next_board, self.expected_board, 'Next() did not return a blank board.')
 
     def test_next__one_live_cell_with_two_live_neighbors_lives(self):
-        starting_cells = [(1,4), (1,5), (1,6)]
+        starting_cells = [(1,1), (1,2), (1,3)]
         life = Life(starting_cells)
 
         next_board = life.next(self.width, self.height)
-        self.expected_board[1][5] = 1
+        self.expected_board[1][2] = 1
 
         self.assertEqual(next_board, self.expected_board, 'Next() did not return a blank board.')
 
