61f298cfbdc75d8d73b96de2f3dc6860fc81c8c9
Michael Patterson <mjpatter88@gmail.com>
2016-04-09 23:18:51 -0500

    Main looks for speed in cmd line args. Defaults to 4 if not provided.

--- life.py
+++ life.py
@@ -59,6 +59,7 @@ if __name__ == '__main__':
     filename = sys.argv[1] if len(sys.argv) >= 2 else 'beacon.txt'
     width = int(sys.argv[2]) if len(sys.argv) >= 3 else 6
     height = int(sys.argv[3]) if len(sys.argv) >= 4 else 6
+    speed = int(sys.argv[4]) if len(sys.argv) >= 5 else 4
 
     starting_cells = []
     with open(filename, 'r') as in_file:
@@ -76,4 +77,4 @@ if __name__ == '__main__':
                 print(col, end='')
             print()
         print("\n\nIteration: {}".format(x))
-        time.sleep(.25)
+        time.sleep(1.0/speed)
