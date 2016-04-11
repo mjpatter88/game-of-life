eb0084bb10b1569b68f77cb6f36bc6dc9a98acfb
Michael Patterson <mjpatter88@gmail.com>
2016-04-11 10:01:24 -0500

    Refactor main method and cleanup code.

--- life_main.py
+++ life_main.py
@@ -2,40 +2,36 @@ from life import Life
 import os
 import sys
 import time
-import pprint
 from ast import literal_eval
 
+filename = sys.argv[1] if len(sys.argv) >= 2 else 'beacon.txt'
+width = int(sys.argv[2]) if len(sys.argv) >= 3 else 6
+height = int(sys.argv[3]) if len(sys.argv) >= 4 else 6
+speed = int(sys.argv[4]) if len(sys.argv) >= 5 else 4
 
-if __name__ == '__main__':
+starting_cells = []
+with open(filename, 'r') as in_file:
+    for line in in_file.readlines():
+        if line[0] == '(':
+            starting_cells.append(literal_eval(line))
 
-    filename = sys.argv[1] if len(sys.argv) >= 2 else 'beacon.txt'
-    width = int(sys.argv[2]) if len(sys.argv) >= 3 else 6
-    height = int(sys.argv[3]) if len(sys.argv) >= 4 else 6
-    speed = int(sys.argv[4]) if len(sys.argv) >= 5 else 4
+life = Life(starting_cells)
+live_char = '+'
 
-    starting_cells = []
-    with open(filename, 'r') as in_file:
-        for line in in_file.readlines():
-            if line[0] == '(':
-                starting_cells.append(literal_eval(line))
+if os.name == 'nt':
+    clear_command = 'cls'
+else:
+    clear_command = 'clear'
 
-    life = Life(starting_cells)
-    live_char = '+'
-
-    if os.name == 'nt':
-        clear_command = 'cls'
-    else:
-        clear_command = 'clear'
-
-    for x in range(1000):
-        os.system(clear_command)
-        board = life.next(width, height)
-        for row in board:
-            for col in row:
-                if col:
-                    print(live_char, end='')
-                else:
-                    print(' ', end='')
-            print()
-        print("\n\nIteration: {}".format(x))
-        time.sleep(1.0/speed)
+for x in range(1000):
+    os.system(clear_command)
+    board = life.next(width, height)
+    for row in board:
+        for col in row:
+            if col:
+                print(live_char, end='')
+            else:
+                print(' ', end='')
+        print()
+    print("\n\nIteration: {}".format(x))
+    time.sleep(1.0/speed)
